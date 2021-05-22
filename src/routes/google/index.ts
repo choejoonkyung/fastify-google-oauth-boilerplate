5e971998be50e4f1f188dc4db33117352262dae0
choejoonkyung <choejunkyung@gmail.com>
2021-05-22 13:53:07 +0900

    feat(swagger): 스웨거 분리

--- src/routes/google/index.ts
+++ src/routes/google/index.ts
@@ -1,28 +1,26 @@
-import { FastifyPluginAsync } from "fastify";
+import { FastifyPluginAsync, RouteShorthandOptions } from "fastify";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 const auth: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get(
-    "/auth",
-    {
-      schema: {
-        tags: ["Google oauth"],
-        description: "구글 oauth 요청 api",
-      },
+  const swagger: RouteShorthandOptions = {
+    schema: {
+      tags: ["Google oauth"],
+      description: "구글 oauth 요청 api",
     },
-    async function (request, reply) {
-      await prisma.user.create({
-        data: {
-          name: "Jk",
-        },
-      });
-      return {
-        google: "auth",
-      };
-    }
-  );
+  };
+
+  fastify.get("/auth", swagger, async function (request, reply) {
+    await prisma.user.create({
+      data: {
+        name: "Jk",
+      },
+    });
+    return {
+      google: "auth",
+    };
+  });
 };
 
 export default auth;
