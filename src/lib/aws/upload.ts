6c425b8c918f8ebb783a3e48a1f2458f478db1f1
choejoonkyung <choejunkyung@gmail.com>
2021-05-27 23:00:07 +0900

    feat(s3 image): s3에 업로드 하는 multer 함수

--- src/lib/aws/upload.ts
+++ src/lib/aws/upload.ts
@@ -1,30 +1,31 @@
-import fs from "fs";
 import AWS from "aws-sdk";
+import multer from "multer";
+import multerS3 from "multer-s3";
+import path from "path";
 
 const { BUCKET_NAME, REGION } = process.env;
 
+if (!BUCKET_NAME || !REGION)
+  throw new Error("환경변수에 버킷이름이 없거나 리전이 없습니다.");
+
 const s3 = new AWS.S3({
-  apiVersion: "2006-03-1",
+  apiVersion: "2006-03-01",
+  params: { Bucket: BUCKET_NAME },
   region: REGION,
 });
 
-export default function uplaod(fileDir: string, targetDir: string) {
-  if (!BUCKET_NAME) throw new Error("BUCKET_NAME이 존재하지 않습니다.");
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: BUCKET_NAME,
+    contentType: multerS3.AUTO_CONTENT_TYPE, // 자동으로 콘텐츠 타입 세팅
+    acl: "public-read",
+    key: (req, file, cb) => {
+      let extension = path.extname(file.originalname);
+      cb(null, "images/" + Date.now().toString() + extension);
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 }, // 용량 제한
+});
 
-  return new Promise((res, rej) => {
-    const fileStream = fs.createReadStream(fileDir);
-    fileStream.on("error", (err) => {
-      return rej(err);
-    });
-    s3.upload(
-      {
-        Bucket: BUCKET_NAME,
-        Key: targetDir,
-      },
-      (err, data) => {
-        if (err) return rej(err);
-        res(data);
-      }
-    );
-  });
-}
+export default upload;
